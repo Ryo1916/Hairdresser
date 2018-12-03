c68e433e9fe1a96a2c36be3f4d3f00b597b4aab4
Ryo1916 <telemon1916@gmail.com>
2018-12-03 20:15:19 +0900

    Move pages/posts_controller to blog folder and set scope at routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
 
-  get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact
-  resources :posts
+
+  scope module: 'blog' do
+    get 'about' => 'pages#about', as: :about
+    get 'contact' => 'pages#contact', as: :contact
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
