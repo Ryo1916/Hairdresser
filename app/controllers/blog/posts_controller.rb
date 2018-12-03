e4737806b53d76f18354afdd7990008c1b0875d9
Ryo1916 <telemon1916@gmail.com>
2018-12-03 22:55:57 +0900

    Remove set_param and strong_parameter

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -1,6 +1,5 @@
 module Blog
   class PostsController < BlogController
-    before_action :set_post, only: [:show]
 
     # GET /posts
     # GET /posts.json
@@ -11,18 +10,9 @@ module Blog
     # GET /posts/1
     # GET /posts/1.json
     def show
+      @post = Post.friendly.find(params[:id])
     end
 
-
-    private
-
-      def set_post
-        @post = Post.friendly.find(params[:id])
-      end
-
-      def post_params
-        params.require(:post).permit(:title, :body, :description, :slug, :banner_image_url)
-      end
   end
 
 end
