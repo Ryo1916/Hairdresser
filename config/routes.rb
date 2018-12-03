4e649f264a1619384683c4a68bd48be6fa961fbb
Ryo1916 <telemon1916@gmail.com>
2018-12-03 21:33:03 +0900

    Set namespace and scope module at routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
   root to: 'blog/posts#index'
 
+  # namespace: URLとcontrollerの両方とも指定のパスになる
+  namespace :author do
+    resources :posts
+  end
+
+  # scope module: controllerのみ指定のパスになる
   scope module: 'blog' do
     get 'about' => 'pages#about', as: :about
     get 'contact' => 'pages#contact', as: :contact
-    resources :posts
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
