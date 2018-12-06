31c78c4d9637158d82afa800218e19cda878ac17
Ryo1916 <telemon1916@gmail.com>
2018-12-06 09:19:18 +0900

    Change devise function (out: registertable, in: trackable)

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,8 +1,8 @@
 class Author < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :registerable, :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable
 
   # association
   has_many :posts
