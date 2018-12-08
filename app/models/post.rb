c6faa9366a347be8b42f29480dab02cbfa3619d1
Ryo1916 <telemon1916@gmail.com>
2018-12-08 13:38:02 +0900

    Set authenticate_author as before action

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,11 @@ class Post < ApplicationRecord
   # association
   belongs_to :author
 
+  # Friendly ID gem
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  # most_recent as if order method
   scope :most_recent, -> { order(id: :desc) }
 
   def should_generate_new_friendly_id?
