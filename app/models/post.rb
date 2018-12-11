49c4c9eee507f1082e4319b668549064d7ea8a5e
Ryo1916 <telemon1916@gmail.com>
2018-12-11 21:16:09 +0900

    Set published scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -24,8 +24,9 @@ class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
-  # most_recent as if order method
+  # scopes
   scope :most_recent, -> { order(id: :desc) }
+  scope :published, -> { where(published: true) }
 
   def should_generate_new_friendly_id?
     title_changed?
