3f593df848ce0b16ac7f0c79bd0e41454d499116
Ryo1916 <telemon1916@gmail.com>
2018-12-15 12:21:40 +0900

    Refactor crazy methods chaining

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -4,13 +4,14 @@ module Blog
     # GET /posts
     # GET /posts.json
     def index
-      @posts = Post.most_recent.published.paginate(page: params[:page], per_page: 24)
+      @posts = Post.published.list_for(params[:page], params[:tag])
     end
 
     # GET /posts/1
     # GET /posts/1.json
     def show
       @post = Post.friendly.find(params[:id])
+      @post.published ? @post : redirect_to(posts_path)
     end
 
   end
