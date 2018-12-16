5f7408f723d24e27b9ed042ad98548f80f4c956c
Ryo1916 <telemon1916@gmail.com>
2018-12-16 22:17:37 +0900

    change scope to use generated views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '30071d1189b7efac6a23ec62a375e7092dc62eae2422747f76cbc5a8425963770afec868c2f5f999475a922d1cc9dc41415fc09d57db7103226fc4af83fa7a86'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
