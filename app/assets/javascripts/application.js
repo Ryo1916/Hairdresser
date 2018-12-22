1e9553d966fc3db9b72ae09a84b106206b3b215f
Ryo1916 <telemon1916@gmail.com>
2018-12-22 14:18:10 +0900

    Hide navbar first

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,22 @@
 //= require tinymce-jquery
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+
+  // hide .navbar first
+  $(".navbar").hide();
+
+  // fade in .navbar
+  $(function () {
+    $(window).scroll(function () {
+      // set distance user needs to scroll before we start fadeIn
+      if ($(this).scrollTop() > 10) {
+        $('.navbar').fadeIn("slow", "swing");
+      } else {
+        $('.navbar').fadeOut("slow", "swing");
+      }
+    });
+  });
+
+});
