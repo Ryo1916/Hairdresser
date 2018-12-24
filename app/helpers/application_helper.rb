3d56060e6c8d8880fb1fcec45152f7e35386a6b7
Ryo1916 <telemon1916@gmail.com>
2018-12-24 17:31:53 +0900

    Set full_title method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '') # 引数のdefault値を空に設定している
+    base_title = "Hairdresser's Website"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
