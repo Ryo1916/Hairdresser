6c8de39afe26a3ac62ac3f8daf65dd9a99d0e381
Ryo1916 <telemon1916@gmail.com>
2019-04-08 19:44:42 +0900

    Change redirect page after sign_in/sign_out/update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   layout 'author'
+
+  private
+
+  def after_sign_in_path_for(resource)
+    authors_posts_path(resource)
+  end
+
+  def after_update_path_for(resource)
+    edit_author_registration_path(resource)
+  end
+
+  def after_sign_out_path_for
+    new_author_session_path
+  end
 end
