c22c7a1a8b2a5f2e5070b1cd937528b291541931
Ryo1916 <telemon1916@gmail.com>
2019-04-08 20:12:51 +0900

    Order posts by created_at

--- app/models/post.rb
+++ app/models/post.rb
@@ -33,7 +33,7 @@ class Post < ApplicationRecord
   }
   scope :paginated_post, ->(page) { most_recent.paginate(page: page, per_page: 6) }
   scope :recent_paginated_post, ->(page) { most_recent.paginate(page: page, per_page: 24) }
-  scope :most_recent, -> { order(published_at: :desc) }
+  scope :most_recent, -> { order(created_at: :desc) }
   scope :with_tag, ->(tag) { tagged_with(tag) if tag.present? }
 
   # Friendly ID gem
