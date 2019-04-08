69587e1de1fa1a6f90e6047488dac1c4d61bd799
Ryo1916 <telemon1916@gmail.com>
2019-04-08 20:57:11 +0900

    Setup image uploader by carrierwave

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class ImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
+  process resize_to_fill: [500, 500]
+
+  version :thumb do
+    process resize_to_fill: [300, 300]
+  end
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w[jpg jpeg png]
+  end
+
+  def filename
+    "#{secure_token}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) || model.instance_variable_set(var, SecureRandom.uuid)
+  end
+end
