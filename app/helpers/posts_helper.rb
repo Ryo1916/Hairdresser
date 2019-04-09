d10a553cd9fc73fa9599e49710d74a2579c735a3
Ryo1916 <telemon1916@gmail.com>
2019-04-09 19:32:19 +0900

    Use the image that is posted instead of banner_image_url
    "わざわざバナーのために別の画像をアップするのは面倒なため、投稿内の画像を表示できるようにした"

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module PostsHelper
+  DEFAULT_IMG = 'https://images.pexels.com/photos/1536356/pexels-photo-1536356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+
+  def parsing_img(text)
+    nodes = Nokogiri::HTML.parse(text)
+    nodes.xpath('//img').blank? ? DEFAULT_IMG : nodes.xpath('//img').attribute('src').value
+  end
 end
