1db49765911dbd6db8d201d5741fe7e3c0082b6d
Ryo1916 <telemon1916@gmail.com>
2019-04-16 19:16:43 +0900

    Bundle exec annotate

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :bigint(8)        not null, primary key
+#  alt        :string(255)
+#  hint       :string(255)
+#  file       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
