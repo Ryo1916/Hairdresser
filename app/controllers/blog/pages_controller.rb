fe3f2790d96e5b2bb45167161456e8458fce57d9
Ryo1916 <telemon1916@gmail.com>
2019-04-26 21:36:10 +0900

    Set 6 tags and order by ascending

--- app/controllers/blog/pages_controller.rb
+++ app/controllers/blog/pages_controller.rb
@@ -5,6 +5,7 @@ module Blog
     def top
       @posts = Post.published.list_for_top(params[:page], params[:tag])
       @author = Author.first
+      @tags = ActsAsTaggableOn::Tag.order(id: :ASC).limit(6)
     end
   end
 end
