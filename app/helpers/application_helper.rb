ddbce6c1bce684be1000a6f0a10e2dfb6e54986d
Ryo1916 <telemon1916@gmail.com>
2019-04-26 21:35:11 +0900

    Set constant images array to display category images on the top page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,18 @@ module ApplicationHelper
   # Constants
   APP_NAME = 'Hairdresser Yuta'
 
+  # To display category images on top page instantly
+  def category_images
+    category_images = [
+      'https://images.pexels.com/photos/1319460/pexels-photo-1319460.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      'https://images.pexels.com/photos/1570807/pexels-photo-1570807.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      'https://images.pexels.com/photos/668196/pexels-photo-668196.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      'https://images.pexels.com/photos/6171/hairstyle-hair-wedding-bride.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      'https://images.pexels.com/photos/112782/pexels-photo-112782.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+      'https://images.pexels.com/photos/4614/woman-morning-bathrobe-bathroom.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+    ]
+  end
+
   # Return web page title
   def full_title(page_title = '')
     base_title = APP_NAME
