13016000acc52c14059c6d432d88c0b464389ba6
Ryo1916 <telemon1916@gmail.com>
2019-05-02 18:49:44 +0900

    Move disqus script to show.html.erb

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -63,21 +63,4 @@ $(document).ready(function(){
       }
     });
   })
-
-  // For disqus
-  /**
-  *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-  *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
-  var disqus_config = function () {
-    this.page.url = '<%= request.original_url %>';  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = '<%= @post.slug %>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-  };
-
-  (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://yuta-arita.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-  })();
-
 });
