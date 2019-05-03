39eb04db2dae07d3aa6ccd04cfef2537d68a8a74
Ryo1916 <telemon1916@gmail.com>
2019-05-03 18:03:46 +0900

    Set most_views instance variable

--- app/controllers/blog/posts_controller.rb
+++ app/controllers/blog/posts_controller.rb
@@ -5,6 +5,7 @@ module Blog
     include Common
 
     before_action :set_author, only: %i[index show]
+    before_action :set_most_views, only: %i[index show]
 
     def index
       @posts = Post.published.list_for_blog(params[:page], params[:tag])
@@ -14,6 +15,14 @@ module Blog
       @post = Post.friendly.find(params[:id])
       @post.published ? @post : redirect_to(posts_path, flash: { alert: 'This post is not published.' })
       @post.punch(request)
+    end
+
+    private
+
+    MAX_DISPLAY_NUMBER_OF_MOST_VIEWED = 10
+
+    def set_most_views
+      @most_views = Post.most_hit.limit(MAX_DISPLAY_NUMBER_OF_MOST_VIEWED)
     end
   end
 end
