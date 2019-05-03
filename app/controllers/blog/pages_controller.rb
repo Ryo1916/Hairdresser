c73e850bbe236a3b18b27ca10dcb12c6ee6bbfc4
Ryo1916 <telemon1916@gmail.com>
2019-05-03 18:05:00 +0900

    Set the constant to avoid using magic number

--- app/controllers/blog/pages_controller.rb
+++ app/controllers/blog/pages_controller.rb
@@ -2,10 +2,12 @@
 
 module Blog
   class PagesController < BlogController
+    MAX_DISPLAY_NUMBER_OF_CATEGORIES = 6
+
     def top
       @posts = Post.published.list_for_top(params[:page], params[:tag])
       @author = Author.first
-      @tags = ActsAsTaggableOn::Tag.order(id: :ASC).limit(6)
+      @tags = ActsAsTaggableOn::Tag.order(id: :ASC).limit(MAX_DISPLAY_NUMBER_OF_CATEGORIES)
     end
   end
 end
