1f358d073987ee7bb504c0fe43ecd259347f3d02
Ryo1916 <telemon1916@gmail.com>
2019-05-04 18:16:22 +0900

    Delete comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,14 @@
 Rails.application.routes.draw do
   root to: 'blog/pages#top'
 
-  # 管理者ログイン／ログアウト用ルーティング
   devise_for :authors
-  # author.rbにregisterable未設定のため、registrationへのルーティング追記
   as :author do
     get 'authors/edit' => 'authors/registrations#edit', as: :edit_user_registration
     put 'author/:id' => 'authors/registrations#update', as: :user_registration
   end
 
-  # 管理者用投稿ページ
   namespace :authors do
     resources :posts do
-      # memberでpostsの7つのリソースに以下2つを追加
       member do
         put 'publish' => 'posts#publish'
         put 'unpublish' => 'posts#unpublish'
@@ -24,7 +20,6 @@ Rails.application.routes.draw do
     post '/tinymce_assets' => 'tinymce_assets#create'
   end
 
-  # 閲覧者用ルーティング
   scope module: 'blog' do
     get 'top' => 'pages#top', as: :top
     get 'posts' => 'posts#index', as: :posts
