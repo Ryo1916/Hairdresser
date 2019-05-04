74dce8d088d41cd87bc6753c09fa8c41dabc1a46
Ryo1916 <telemon1916@gmail.com>
2019-05-04 18:15:09 +0900

    Change the redirect page when updating account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,10 +11,6 @@ class ApplicationController < ActionController::Base
     authors_posts_path(resource)
   end
 
-  def after_update_path_for(resource)
-    edit_author_registration_path(resource)
-  end
-
   def after_sign_out_path_for(_resource)
     new_author_session_path
   end
--- app/controllers/authors/registrations_controller.rb
+++ app/controllers/authors/registrations_controller.rb
@@ -13,5 +13,9 @@ module Authors
     def configure_account_update_params
       devise_parameter_sanitizer.permit(:account_update, keys: [:name])
     end
+
+    def after_update_path_for(resource)
+      authors_posts_path(resource)
+    end
   end
 end
