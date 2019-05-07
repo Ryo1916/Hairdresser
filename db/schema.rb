905c936b573dcadb899b08a6a478d1f3bb78e86d
Ryo1916 <telemon1916@gmail.com>
2019-05-07 18:55:38 +0900

    Add lockable columns to Author

--- db/schema.rb
+++ db/schema.rb
@@ -27,10 +27,14 @@ ActiveRecord::Schema.define(version: 20190502100509) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_authors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_authors_on_unlock_token", unique: true
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
