4689cc92c457aee4d91be5dee8a1f4691b267f47
Ryo1916 <telemon1916@gmail.com>
2019-06-12 00:40:05 +0900

    Set application helper spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,16 +2,16 @@
 
 require 'rails_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ApplicationHelper. For example:
-#
-# describe ApplicationHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe ApplicationHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'full_title' do
+    it 'returns only base_title' do
+      expect(helper.full_title).to eq(Constants::APP_NAME)
+    end
+
+    it 'returns base_title with page_title' do
+      page_title = 'Posts'
+      expect(helper.full_title(page_title: page_title))
+        .to eq("#{page_title} | #{Constants::APP_NAME}")
+    end
+  end
 end
