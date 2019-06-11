273074c209e3eb20334697e89eb4f579bdaad1af
Ryo1916 <telemon1916@gmail.com>
2019-06-11 20:51:47 +0900

    Generate post helper spec

---
+++ spec/helpers/posts_helper_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TagsHelper, type: :helper do
+  describe 'display_day_published' do
+    xit 'returns published day' do
+    end
+
+    xit "returns 'Not published yet.'" do
+    end
+  end
+end
