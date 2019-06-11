fbf4454c8fdea615e3e5c3ce6b732d9fa808d9e6
Ryo1916 <telemon1916@gmail.com>
2019-06-12 01:26:07 +0900

    Set post helper spec

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -3,11 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe TagsHelper, type: :helper do
-  describe 'display_day_published' do
-    xit 'returns published day' do
+  let!(:author) { create(:author) }
+  let!(:post) { create(:post, author: author) }
+  let!(:post_with_image) { create(:post, author: author, body: "<img src=\'/uploads/image/file/2/default.jpeg\'/>") }
+  let!(:image) { create(:image) }
+
+  describe 'parsing_img' do
+    it 'returns the default image' do
+      expect(helper.parsing_img(text: post.body))
+        .to match('/uploads/image/file/1/')
     end
 
-    xit "returns 'Not published yet.'" do
+    it 'returns the uploaded image' do
+      expect(helper.parsing_img(text: post_with_image.body))
+        .to match('/uploads/image/file/2')
+    end
+  end
+
+  describe 'display_day_published' do
+    it 'returns published day' do
+      post.publish
+      expect(helper.display_day_published(post: post))
+        .to eq("Published #{post.published_at.strftime('%-b %-d, %Y')}")
+    end
+
+    it "returns 'Not published yet.'" do
+      expect(helper.display_day_published(post: post))
+        .to eq('Not published yet.')
     end
   end
 end
