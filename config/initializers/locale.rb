74e6b974f2d911332878f48bcc7b2b6ea298ccf7
Ryo1916 <telemon1916@gmail.com>
2019-06-13 16:50:28 +0900

    Set available locales and the default locale

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -4,4 +4,7 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
 
 # set available locales as whitelist
-I18n.available_locales = %i[:en :ja]
+I18n.available_locales = %i[en ja]
+
+# change default locale to japanese
+I18n.default_locale = :ja
