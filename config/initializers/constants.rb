886784e20e0b07ce9bd11050a3ed680f7d8fe94e
Ryo1916 <telemon1916@gmail.com>
2019-06-19 14:11:21 +0900

    Display 8 new posts at top page

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,7 +4,7 @@ module Constants
   # TODO: separate production and others
   APP_NAME = 'Hairdresser Yuta'
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  MAX_DISPLAY_POSTS_NUM_FOR_TOP_PAGE = 6
+  MAX_DISPLAY_POSTS_NUM_FOR_TOP_PAGE = 8
   MAX_DISPLAY_CATEGORIES_NUM_FOR_TOP_PAGE = 6
   MAX_DISPLAY_POSTS_NUM_FOR_INDEX_PAGE = 24
   MAX_DISPLAY_NUM_FOR_POPULAR_POSTS = 10
