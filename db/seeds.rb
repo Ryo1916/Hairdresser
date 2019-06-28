f82c3b09f545e8a738b682cf3eb4191238c55042
Ryo1916 <telemon1916@gmail.com>
2019-06-28 12:38:40 +0900

    Change seed to be responsive images

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ Author.create!( name:                  "yuta arita",
 
 # posts
 Post.create!( title:            "This is my first post on this blog!",
-              body:             "<p>hi world, this is my first post on this blog. I'm a Japanese hairdresser.</p>\r\n<p><img src=\"https://images.pexels.com/photos/1321916/pexels-photo-1321916.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\" alt=\"\" width=\"408\" height=\"544\" /></p>",
+              body:             "<p>hi world, this is my first post on this blog. I'm a Japanese hairdresser.</p>\r\n<p><img class=\"img-fluid\" src=\"https://images.pexels.com/photos/1321916/pexels-photo-1321916.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\" alt=\"\" width=\"408\" height=\"544\" /></p>",
               slug:             "",
               author_id:        1,
               published:        true,
@@ -23,7 +23,7 @@ Post.create!( title:            "This is my first post on this blog!",
               impressions_count: 0 )
 
 Post.create!( title:            "グリーンはオススメです",
-              body:             "<p>グリーンはなかなかエグいと思われる方も多いと思いますが、仕上がりは意外にスモーキーになります。</p>\r\n<p>想像されているようなエグい感じにはなりません。今年の夏オススメの色です！</p>\r\n<p><img src=\"https://arine.akamaized.net/uploads/photo/external_photo/data/316244/xlarge_c5c46a6e-f7c9-4594-a95c-c92f9814cfa4.jpg\" alt=\"green color model\" width=\"408\" height=\"544\" /></p>\r\n<p>もちろん当店でも扱っていますので、気になりましたらぜひお尋ねください！</p>\r\n<p>&nbsp;</p>",
+              body:             "<p>グリーンはなかなかエグいと思われる方も多いと思いますが、仕上がりは意外にスモーキーになります。</p>\r\n<p>想像されているようなエグい感じにはなりません。今年の夏オススメの色です！</p>\r\n<p><img class=\"img-fluid\" src=\"https://arine.akamaized.net/uploads/photo/external_photo/data/316244/xlarge_c5c46a6e-f7c9-4594-a95c-c92f9814cfa4.jpg\" alt=\"green color model\" width=\"408\" height=\"544\" /></p>\r\n<p>もちろん当店でも扱っていますので、気になりましたらぜひお尋ねください！</p>\r\n<p>&nbsp;</p>",
               slug:             "",
               author_id:        1,
               published:        true,
@@ -32,7 +32,7 @@ Post.create!( title:            "グリーンはオススメです",
 
 for i in 1..6 do
   Post.create!( title: "色を変えました part-#{i}",
-                body: "<p>今回はこんな感じ。あまり変わっていないように見えますが、よく観察するとだいぶ変わっていることがわかるかと思います。</p>\r\n<p><img src=\"https://images.pexels.com/photos/973401/pexels-photo-973401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260\" alt=\"\" width=\"408\" height=\"544\" /></p>",
+                body: "<p>今回はこんな感じ。あまり変わっていないように見えますが、よく観察するとだいぶ変わっていることがわかるかと思います。</p>\r\n<p><img class=\"img-fluid\" src=\"https://images.pexels.com/photos/973401/pexels-photo-973401.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260\" alt=\"\" width=\"408\" height=\"544\" /></p>",
                 slug: "",
                 author_id: 1,
                 published: true,
